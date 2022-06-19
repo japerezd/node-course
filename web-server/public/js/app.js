c758222c6afe24ac06b82addcf666b48655ec0b5
Jorge Perez <jorge_alberto1997@hotmail.com>
2022-06-19 00:14:13 -0500

    Make HTTP request using fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,18 @@
 console.log('Client side JS file is loaded!');
+
+fetch('http://puzzle.mead.io/puzzle').then(response => {
+  response.json().then(data => {
+    console.log(data);
+  })
+});
+
+fetch('http://localhost:3000/weather?address=Veracruz').then(response => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  })
+})
