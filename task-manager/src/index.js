8228b1043ea03feec69575cbb99be643ab1e9b32
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-06-28 17:27:18 -0500

    Create resource to create task

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,8 +1,8 @@
-const e = require('express');
 const express = require('express');
 // File is running like the following
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,12 +12,27 @@ app.use(express.json());
 
 app.post('/users', (req, res) => {
   const user = new User(req.body);
-  user.save().then(() => {
-    res.send(user);
-  }).catch(e => {
-    res.status(400).send(e);
-  })
-})
+  user
+    .save()
+    .then(() => {
+      res.status(201).send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
   console.log('Server is up on port: ' + port);
