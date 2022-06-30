d80be16e5d9d3689b8552fa2c1353f59a6bb2766
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-06-29 21:45:36 -0500

    Update task by id

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -84,7 +84,7 @@ app.patch('/users/:id', async (req, res) => {
   if (!isValidOperation) {
     return res.status(400).send({ error: 'Invalid updates!' });
   }
-  
+
   try {
     // new:true will return the new updated item, and not the old one
     const user = await User.findByIdAndUpdate(req.params.id, req.body, {
@@ -97,6 +97,33 @@ app.patch('/users/:id', async (req, res) => {
     }
 
     res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.patch('/tasks/:id',  async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
   } catch (e) {
     res.status(400).send(e);
   }
