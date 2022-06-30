6674b6dcd08c988fcf23b0c1229dd847e2e49bbf
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-06-29 21:53:51 -0500

    Delete task and user by id

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -20,6 +20,20 @@ app.post('/users', async (req, res) => {
   }
 });
 
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+      res.status(500).send();
+  }
+});
+
 app.get('/users', async (req, res) => {
   try {
     const users = await User.find({});
@@ -63,6 +77,7 @@ app.get('/tasks/:id', async (req, res) => {
     res.status(500).send();
   }
 });
+
 
 app.post('/tasks', async (req, res) => {
   try {
@@ -126,6 +141,20 @@ app.patch('/tasks/:id',  async (req, res) => {
     res.send(task);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (e) {
+      res.status(500).send();
   }
 });
 
