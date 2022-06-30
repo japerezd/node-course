5bfc180bfa3fdf5a66ce69901489b96f81b632d5
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-06-30 12:24:27 -0500

    Accept authentication tokens

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,8 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
 // parse incoming json to an object to access them in req handler
 app.use(express.json());
 app.use(userRouter);
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -66,13 +67,9 @@ router.delete('/users/:id', async (req, res) => {
   }
 });
 
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
+// Passing auth middleware as second parameter
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get('/users/:id', async (req, res) => {
