45ff431eb5c91e633046a337bf37a185b5855730
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-06-30 22:07:51 -0500

    Authenticate user endpoints

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -48,7 +48,7 @@ router.post('/users/logoutAll', auth, async (req, res) => {
   }
 })
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'email', 'password', 'age'];
   const isValidOperation = updates.every((update) =>
@@ -60,32 +60,21 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findById(req.params.id);
-    
-    updates.forEach(update => user[update] = req.body[update]);
-    await user.save();
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
+    updates.forEach(update => req.user[update] = req.body[update]);
+    await req.user.save();
+    res.send(req.user);
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
+    // method from mongoose
+    await req.user.remove();
+    res.send(req.user);
   } catch (e) {
-      res.status(500).send();
+    res.status(500).send();
   }
 });
 
