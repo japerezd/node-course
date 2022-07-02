437b8f340ce9b1e6284dac61aa6ce34622a30bbc
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-07-01 23:17:34 -0500

    Validate file uploads

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -98,7 +98,18 @@ router.get('/users/:id', async (req, res) => {
 });
 
 const upload = multer({
-  dest: 'avatars'
+  dest: 'avatars',
+  limits: {
+    fileSize: 1000000
+  },
+
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please provide an image!'));
+    }
+
+    cb(undefined, true);
+  }
 });
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
