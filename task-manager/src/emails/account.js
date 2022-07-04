7684940d6b0a4ee1e0347b83bb94f64a2b0667c6
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-07-04 13:50:55 -0500

    Add dev env

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,8 +1,8 @@
 
 const mailgun = require("mailgun-js");
 
-const api_key = '12370b55dcb2cb6f7bc0b29fafccd37c-1b8ced53-09bf6d8e';
-const DOMAIN = 'sandbox75cc592c7e544eb9a58c366ccdbf4c4b.mailgun.org';
+const api_key = process.env.MAILGUN_API_KEY;
+const DOMAIN = process.env.MAILGUN_DOMAIN;
 
 const mg = mailgun({apiKey: api_key, domain: DOMAIN});
 
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,7 +5,7 @@ const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // parse incoming json to an object to access them in req handler
 app.use(express.json());
