316e6f729983c094d78139a5ae6df026a98d759f
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-07-05 10:36:22 -0500

    Test asynchronous code

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -1,13 +1,28 @@
-const calculateTip = (total, tipPercent = .25) =>  (total * tipPercent) + total;
+const calculateTip = (total, tipPercent = 0.25) => total * tipPercent + total;
 
 const fahrenheitToCelsius = (temp) => {
-  return (temp - 32) / 1.8
-}
+  return (temp - 32) / 1.8;
+};
 
 const celsiusToFahrenheit = (temp) => {
-  return (temp * 1.8) + 32
-}
+  return temp * 1.8 + 32;
+};
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) {
+        return reject('Numbers must be non-negative');
+      }
+
+      resolve(a + b);
+    }, 2000);
+  });
+};
 
 module.exports = {
-  calculateTip, fahrenheitToCelsius, celsiusToFahrenheit
-}
+  calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit,
+  add,
+};
--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -2,6 +2,7 @@ const {
   calculateTip,
   celsiusToFahrenheit,
   fahrenheitToCelsius,
+  add
 } = require('../src/math');
 
 test('Should calculate total with tip', () => {
@@ -14,13 +15,32 @@ test('should calculate total with default tip', () => {
   expect(total).toBe(12.5);
 });
 
-
-test('should convert 32 F to 0 C', () => { 
+test('should convert 32 F to 0 C', () => {
   const fahrenheitToC = fahrenheitToCelsius(32);
-  expect(fahrenheitToC).toBe(0)
- });
+  expect(fahrenheitToC).toBe(0);
+});
 
- test('should convert O C to 32 F', () => { 
+test('should convert O C to 32 F', () => {
   const celsiusToF = celsiusToFahrenheit(0);
-  expect(celsiusToF).toBe(32)
- })
+  expect(celsiusToF).toBe(32);
+});
+
+// parameter "done" is called whatever we want
+// test('async test demo', (done) => {
+//   setTimeout(() => {
+//     expect(1).toBe(2);
+//     done();
+//   }, 2000);
+// });
+
+test('should add two numbers', (done) => {
+  add(2, 3).then(sum => {
+    expect(sum).toBe(5);
+    done();
+  })
+});
+
+test('should add two numbers async/await', async () => {
+  const sum = await add(10, 22);
+  expect(sum).toBe(32);
+});
