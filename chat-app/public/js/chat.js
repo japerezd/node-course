01574b7b73a0a6ddd73c6346682212500a325a3e
Jorge Pérez <jorge_alberto1997@hotmail.com>
2022-07-11 10:25:01 -0500

    :sparkles: Broadcast events

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,10 +1,14 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-  console.log('The count has been updated!', count);
-});
-
-document.querySelector('#increment').addEventListener('click', () => {
-  console.log('clicked');
-  socket.emit('increment');
+socket.on('message', (message) => {
+  console.log(message);
 })
+
+const form = document.querySelector('#message-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.message.value;
+  socket.emit('sendMessage', message);
+});
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -13,16 +13,20 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 // app.use(express.json());
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on('connection', (socket) => {
   console.log('New WebSocket connection');
 
-  socket.emit('countUpdated', count);
-  socket.on('increment', () => {
-    count++
-    // socket.emit('countUpdated', count);
-    io.emit('countUpdated', count)
+  socket.emit('message', 'Welcome!')
+  // Send message to everybody except for himself
+  socket.broadcast.emit('message', 'A new user has joined!')
+
+  socket.on('sendMessage', message => {
+    io.emit('message', message);
+  });
+
+  // built in event (disconnect)
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!')
   })
 })
 
